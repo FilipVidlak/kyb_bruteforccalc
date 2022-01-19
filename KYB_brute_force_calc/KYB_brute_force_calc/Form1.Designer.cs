aa00ac178ffdba09c80b738535af6a2a714b9208
Filip Vidlák <vidlakf.03@spst.eu>
2022-01-19 21:07:46 +0100

    finish

--- KYB_brute_force_calc/KYB_brute_force_calc/Form1.Designer.cs
+++ KYB_brute_force_calc/KYB_brute_force_calc/Form1.Designer.cs
@@ -48,9 +48,10 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.BackColor = System.Drawing.Color.Transparent;
             this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.checkBox1.Location = new System.Drawing.Point(81, 76);
+            this.checkBox1.Location = new System.Drawing.Point(108, 94);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(151, 19);
+            this.checkBox1.Size = new System.Drawing.Size(175, 22);
             this.checkBox1.TabIndex = 0;
             this.checkBox1.Text = "Malá písmena (a-z)";
             this.checkBox1.UseVisualStyleBackColor = false;
@@ -60,9 +61,10 @@
             this.checkBox2.AutoSize = true;
             this.checkBox2.BackColor = System.Drawing.Color.Transparent;
             this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.checkBox2.Location = new System.Drawing.Point(81, 123);
+            this.checkBox2.Location = new System.Drawing.Point(108, 151);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(155, 19);
+            this.checkBox2.Size = new System.Drawing.Size(182, 22);
             this.checkBox2.TabIndex = 1;
             this.checkBox2.Text = "Velká písmena (A-Z)";
             this.checkBox2.UseVisualStyleBackColor = false;
@@ -72,9 +74,10 @@
             this.checkBox3.AutoSize = true;
             this.checkBox3.BackColor = System.Drawing.Color.Transparent;
             this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.checkBox3.Location = new System.Drawing.Point(81, 171);
+            this.checkBox3.Location = new System.Drawing.Point(108, 210);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(4);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(104, 19);
+            this.checkBox3.Size = new System.Drawing.Size(122, 22);
             this.checkBox3.TabIndex = 2;
             this.checkBox3.Text = "Číslice (0-9)";
             this.checkBox3.UseVisualStyleBackColor = false;
@@ -84,18 +87,20 @@
             this.checkBox4.AutoSize = true;
             this.checkBox4.BackColor = System.Drawing.Color.Transparent;
             this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.checkBox4.Location = new System.Drawing.Point(81, 215);
+            this.checkBox4.Location = new System.Drawing.Point(108, 265);
+            this.checkBox4.Margin = new System.Windows.Forms.Padding(4);
             this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(163, 19);
+            this.checkBox4.Size = new System.Drawing.Size(191, 22);
             this.checkBox4.TabIndex = 3;
             this.checkBox4.Text = "Speciální znaky (*!@)";
             this.checkBox4.UseVisualStyleBackColor = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(438, 80);
+            this.textBox1.Location = new System.Drawing.Point(584, 98);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 4;
             // 
             // label1
@@ -103,9 +108,10 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(366, 84);
+            this.label1.Location = new System.Drawing.Point(488, 103);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.Size = new System.Drawing.Size(83, 20);
             this.label1.TabIndex = 5;
             this.label1.Text = "Heslo je z";
             // 
@@ -114,17 +120,19 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(544, 81);
+            this.label2.Location = new System.Drawing.Point(725, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.Size = new System.Drawing.Size(53, 20);
             this.label2.TabIndex = 6;
             this.label2.Text = "znaků";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(438, 149);
+            this.textBox2.Location = new System.Drawing.Point(584, 183);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
             this.textBox2.TabIndex = 7;
             // 
             // label3
@@ -132,20 +140,22 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(279, 150);
+            this.label3.Location = new System.Drawing.Point(372, 185);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(153, 16);
+            this.label3.Size = new System.Drawing.Size(193, 20);
             this.label3.TabIndex = 8;
             this.label3.Text = "Počet pokusů za vteřinu:";
             // 
             // button1
             // 
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Noto Sans Georgian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.Window;
-            this.button1.Location = new System.Drawing.Point(424, 269);
+            this.button1.Location = new System.Drawing.Point(565, 331);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 48);
+            this.button1.Size = new System.Drawing.Size(152, 59);
             this.button1.TabIndex = 9;
             this.button1.Text = "Vypočítej";
             this.button1.UseVisualStyleBackColor = true;
@@ -154,10 +164,11 @@
             // button2
             // 
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Font = new System.Drawing.Font("Noto Sans Georgian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(81, 269);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(108, 331);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 48);
+            this.button2.Size = new System.Drawing.Size(159, 59);
             this.button2.TabIndex = 10;
             this.button2.Text = "Smaž";
             this.button2.UseVisualStyleBackColor = true;
@@ -167,20 +178,22 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label4.ForeColor = System.Drawing.Color.Gold;
-            this.label4.Location = new System.Drawing.Point(366, 237);
+            this.label4.Location = new System.Drawing.Point(103, 504);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.Size = new System.Drawing.Size(30, 25);
             this.label4.TabIndex = 11;
             this.label4.Text = "...";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(642, 488);
+            this.ClientSize = new System.Drawing.Size(856, 601);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -195,6 +208,9 @@
             this.Controls.Add(this.checkBox1);
             this.ForeColor = System.Drawing.Color.White;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(874, 648);
+            this.MinimumSize = new System.Drawing.Size(874, 648);
             this.Name = "Form1";
             this.Text = "Brute Force Calculator";
             this.ResumeLayout(false);
--- KYB_brute_force_calc/KYB_brute_force_calc/Form1.cs
+++ KYB_brute_force_calc/KYB_brute_force_calc/Form1.cs
@@ -19,30 +19,61 @@ namespace KYB_brute_force_calc
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (checkBox1.Checked) {
-               int malapismena = 26;
+            try
+            {
+                label4.Text = "";
+                int pocetznaku = Int32.Parse(textBox1.Text);
+                int pocetpokusu = Int32.Parse(textBox2.Text);
+                double kombinace = 0;
+                double year = 0, month = 0, day = 0, hour = 0, min = 0, second = 0;
+                if (checkBox1.Checked == true)
+                {
+                    kombinace += 26;
+                }
+                if (checkBox2.Checked == true)
+                {
+                    kombinace += 26;
+                }
+                if (checkBox3.Checked == true)
+                {
+                    kombinace += 10;
+                }
+                if (checkBox4.Checked == true)
+                {
+                    kombinace += 33;
+                }
+                kombinace = Math.Round(Math.Pow(kombinace, pocetznaku) / pocetpokusu);
+                if (kombinace >= 60) { min = Math.Floor(kombinace / 60); kombinace -= min * 60; second = kombinace; }
+                if (min >= 60) { hour = Math.Floor(min / 60); min -= hour * 60; }
+                if (hour >= 24) { day = Math.Floor(hour / 60); hour -= day * 60; }
+                if (day >= 30) { month = Math.Floor(day / 60); day -= month * 60; }
+                if (month >= 12) { year = Math.Floor(month / 60); month -= year * 60; }
+                if (year >= 10000) { label4.Text = "Nikdy nerozluštíš prostě, i kvantovej počítač je na to krátkej"; }
+                else
+                {
+                    label4.Text = year.ToString() + " rok/y/ů " + month.ToString() + " měsíc/e/ů " + hour.ToString() + " hodin/a/y " + min.ToString() + " minut/a/y " + second.ToString() + " vteřin/a/y ";
+                }
+            } 
+            catch
+            {
+                MessageBox.Show("Nezadal jsi žádnou hodnotu nebo jsi zadal špatnou hodnotu!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Clear();
+                textBox2.Clear();
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
             }
-            if (checkBox2.Checked) {
-                int velkapismena = 26;
-            }
-            if (checkBox3.Checked) {
-                int cislice = 10;
-            }
-            if (checkBox4.Checked) {
-                int specialznaky = 33;
-            }
-            int pocetznaku = Int32.Parse(textBox1.Text);
-
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
-            checkBox1.Checked = false;
-            checkBox2.Checked = false;
-            checkBox3.Checked = false;
-            checkBox4.Checked = false;
-            foreach(Control objekt in this.Controls) {
-                if(objekt is CheckBox) {
+            label4.Text = "";
+            textBox1.Clear();
+            textBox2.Clear();
+            foreach (Control objekt in this.Controls)
+            {
+                if (objekt is CheckBox)
+                {
                     (objekt as CheckBox).Checked = false;
                 }
             }
